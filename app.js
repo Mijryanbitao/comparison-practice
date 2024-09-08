3bb77ac2bec5d86f7f5b94588ad3851f1b2dbf83
Mijryanbitao <mijryanb@gmail.com>
2024-09-08 13:09:02 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+let x = 10;
+let y = 10;
+
+//Use comparison operator and store the results in variables
+let isXGreaterThanY = x > y;
+let isXLessThanY = x < y;
+let isXEqualToY = x === y;
+let isXNotEqualToY = x !== y;
+
+console.log("is X greater than Y?" , isXGreaterThanY);
+console.log("is X less than Y?" , isXLessThanY);
+console.log("is X equal to Y?" , isXEqualToY);
+console.log("is X not equal to Y?" , isXNotEqualToY);
